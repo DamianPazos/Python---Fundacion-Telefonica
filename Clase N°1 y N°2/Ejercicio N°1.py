61250875606872232f6fa2f4e5fe5cc4ef6ce38f
Pazos Damian <dpazos@andreani.com>
2022-06-05 01:30:42 -0300

    Ejercicio N°1 - Clase N°1 y N°2

---
+++ Clase N°1 y N°2/Ejercicio N°1.py
@@ -0,0 +1,19 @@
+# Pedir el ingreso por teclado de 3 valores numéricos entre 1 y 10 
+# correspondientes a las notas de un alumno.
+# En base al promedio final de las tres notas, mostrar un mensaje por pantalla 
+# que indique si el alumno promociona la materia (nota final 7,8,9 o 10), debe 
+# rendir final (nota final 4,5 o 6) o recursa (nota 1,2,3).
+
+nota_uno = int(input('Ingrese una nota entre 1 y 10: '))
+nota_dos = int(input('Ingrese una nota entre 1 y 10: '))
+nota_tres = int(input('Ingrese una nota entre 1 y 10: '))
+
+promedio = (nota_uno + nota_dos + nota_tres)/3
+
+if promedio >= 7 :
+    print("Promociona la materia")
+elif 7 > promedio >= 4 :
+    print("Debe rendir final")
+else:
+    print("Recursa")
+
