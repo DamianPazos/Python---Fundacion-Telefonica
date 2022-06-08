c6d79dfc46e21a19f14ea3504003f0793aab789e
Pazos Damian <dpazos@andreani.com>
2022-06-08 16:03:29 -0300

    Ejercicio Nº12 - Clase Nº1 y Nº2

--- Clase N°1 y N°2/Ejercicio N°12.py
+++ Clase N°1 y N°2/Ejercicio N°12.py
@@ -3,17 +3,20 @@
 # Un número primo es aquel que solo tiene como divisores enteros (resto igual 
 # a cero) al número 1 y a sí mismo, por ejemplo, el número 5.
 
+def numero_primo(numero):
+    bandera_primo = True
+
+    for x in range(9):
+        if x != 1 and x != 0:
+            if numero % x == 0 and numero != x:
+
+                bandera_primo = False
+                break
+
+    if bandera_primo == False:
+        return 'El numero ingresado no es un numero primo'
+    else:
+        return 'El numero ingresado es un numero primo'
+    
 numero_ingresado = int(input('Ingrese un numero entero: '))
-bandera_primo = True
-
-for x in range(9):
-    if x != 1 and x != 0:
-        if numero_ingresado % x == 0 and numero_ingresado != x:
-            
-            bandera_primo = False
-            break
-
-if bandera_primo == False:
-    print('El numero ingresado no es un numero primo')
-else:
-    print('El numero ingresado es un numero primo')
+print(numero_primo(numero_ingresado))
---
+++ Clase N°1 y N°2/Ejercicio Nº13.py
@@ -0,0 +1,3 @@
+# Escribir una función que encuentre los números primos comprendidos entre 
+# dos números enteros ingresados por teclado.
+
