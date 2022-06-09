5bb002c434edafd667265f2e80f100468e27f76c
Pazos Damian <dpazos@andreani.com>
2022-06-09 08:02:35 -0300

    Ejercicio N°13

--- Clase N°1 y N°2/Ejercicio Nº13.py
+++ Clase N°1 y N°2/Ejercicio Nº13.py
@@ -1,3 +1,20 @@
 # Escribir una función que encuentre los números primos comprendidos entre 
 # dos números enteros ingresados por teclado.
 
+def numeros_primos (numero_uno,numero_dos):
+    
+    list_numeros_primos = []
+    
+    for i in range(numero_uno,numero_dos):
+        bandera_primo = True
+        for x in range(2,10):
+            if i % x == 0 and i != x:
+                bandera_primo = False
+                break
+        
+        if bandera_primo == True:
+            list_numeros_primos.append(i)
+            
+    return list_numeros_primos
+            
+print(numeros_primos(10,20))
