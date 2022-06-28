620c603b36442297c3dd3c9198cfb9b47f97d4c7
Pazos Damian <damian_ariel_pazos@hotmail.com>
2022-06-28 01:46:27 -0300

    Ejercicio extra N°3 - Modulo N°1

---
+++ Ejercicios Python extra - Modulo N°1/Ejercicio N°3.py
@@ -0,0 +1,14 @@
+# Dado un string, mostrar por pantalla unicamente las vocales en formato Mayuscula
+# Por ejemplo, el string `"Este es un string de prueba"` obtendra como output `['E', 'E', 'E', 'U', 'I', 'E', 'U', 'E', 'A']`
+
+cadena = input('Ingrese una palabra o una frase: ')
+cadena_vocales = []
+vocales = ('a','A','e','E','i','I','o','O','u','U')
+
+for letra in cadena:    
+    if letra in vocales:
+        letra_mayus = letra.upper()
+        cadena_vocales.append(letra_mayus)
+
+print(f'Las vocales son: {cadena_vocales}')
+    
