12e41e2ce9dd80b20d2a75a8fe0248528ff71f75
Pazos Damian <damian_ariel_pazos@hotmail.com>
2022-07-13 16:49:02 -0300

    Ejercicio N°2 TF - Modulo N°2

--- Ejercicios Python Trabajo Final - Modulo N°2/Ejercicio N°2.py
+++ Ejercicios Python Trabajo Final - Modulo N°2/Ejercicio N°2.py
@@ -3,9 +3,11 @@
 # promedio de todos los números contenidos en la lista.
 # No usar funciones tradicionales
 
+from functools import reduce
+
 lista_numeros = []
 
 for i in range(100):
     lista_numeros.append(i + 1)
 
-
+promedio = float(reduce(lambda x,y: x + y,lista_numeros)/len(lista_numeros))
